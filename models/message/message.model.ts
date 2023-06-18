e9be5f8c0769a6d0ed899435f9b48e4e4bb637b6
spelar <spelar7@gmail.com>
2023-06-18 23:53:55 +0900

    메시지 조회 API 추가

--- models/message/message.model.ts
+++ models/message/message.model.ts
@@ -1,6 +1,7 @@
 import CustomServerError from '@/controllers/error/custom_server_error';
 import FirebaseAdmin from '../firebase_admin';
 import { firestore } from 'firebase-admin';
+import { InMessage, InMessageServer } from './in_message';
 
 const MEMBER_COL = 'members';
 const MSG_COL = 'messages';
@@ -45,8 +46,33 @@ async function post({
   });
 }
 
+async function list({ uid }: { uid: string }) {
+  const memberRef = Firestore.collection(MEMBER_COL).doc(uid);
+  const listData = await Firestore.runTransaction(async (transaction) => {
+    const memberDoc = await transaction.get(memberRef);
+    if (memberDoc.exists === false) {
+      throw new CustomServerError({ statusCode: 400, message: '존재하지 않는 사용자' });
+    }
+    const messageCol = memberRef.collection(MSG_COL);
+    const messageColDoc = await transaction.get(messageCol);
+    const data = messageColDoc.docs.map((mv) => {
+      const docData = mv.data() as Omit<InMessageServer, 'id'>;
+      const returnData = {
+        ...docData,
+        id: mv.id,
+        createAt: docData.createAt.toDate().toISOString(),
+        replyAt: docData.replyAt ? docData.replyAt.toDate().toISOString() : undefined,
+      } as InMessage;
+      return returnData;
+    });
+    return data;
+  });
+  return listData;
+}
+
 const MessageModel = {
   post,
+  list,
 };
 
 export default MessageModel;
---
+++ pages/api/messages.list.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import checkSupportMethod from '@/controllers/error/check_support_method';
+import handleError from '@/controllers/error/handle_error';
+import MessageCtrl from '@/controllers/message.ctrl';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  const supportMethod = ['GET'];
+  try {
+    checkSupportMethod(supportMethod, method);
+    await MessageCtrl.list(req, res);
+  } catch (err) {
+    console.error(err);
+    handleError(err, res);
+  }
+}
