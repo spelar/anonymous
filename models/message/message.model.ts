146764929c0f017dd1bdd35f176cee92016744a2
spelar <spelar7@gmail.com>
2023-07-19 23:25:00 +0900

    메시지 단건 조회 API 추가

--- models/message/message.model.ts
+++ models/message/message.model.ts
@@ -70,6 +70,29 @@ async function list({ uid }: { uid: string }) {
   return listData;
 }
 
+async function get({ uid, messageId }: { uid: string; messageId: string }) {
+  const memberRef = Firestore.collection(MEMBER_COL).doc(uid);
+  const messageRef = Firestore.collection(MEMBER_COL).doc(uid).collection(MSG_COL).doc(messageId);
+  const data = await Firestore.runTransaction(async (transaction) => {
+    const memberDoc = await transaction.get(memberRef);
+    const messageDoc = await transaction.get(messageRef);
+    if (memberDoc.exists === false) {
+      throw new CustomServerError({ statusCode: 400, message: '존재하지 않는 사용자' });
+    }
+    if (messageDoc.exists === false) {
+      throw new CustomServerError({ statusCode: 400, message: '존재하지 않는 문서' });
+    }
+    const messageData = messageDoc.data() as InMessageServer;
+    return {
+      ...messageData,
+      id: messageId,
+      createAt: messageData.createAt.toDate().toISOString(),
+      replyAt: messageData.replyAt ? messageData.replyAt.toDate().toISOString() : undefined,
+    };
+  });
+  return data;
+}
+
 async function postReply({ uid, messageId, reply }: { uid: string; messageId: string; reply: string }) {
   const memberRef = Firestore.collection(MEMBER_COL).doc(uid);
   const messageRef = Firestore.collection(MEMBER_COL).doc(uid).collection(MSG_COL).doc(messageId);
@@ -93,6 +116,7 @@ async function postReply({ uid, messageId, reply }: { uid: string; messageId: st
 const MessageModel = {
   post,
   list,
+  get,
   postReply,
 };
 
--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -87,6 +87,26 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
     }
   }
 
+  async function fetchMessageInfo({ uid, messageId }: { uid: string; messageId: string }) {
+    try {
+      const resp = await fetch(`/api/messages.info?uid=${uid}&messageId=${messageId}`);
+      if (resp.status === 200) {
+        const data: InMessage = await resp.json();
+        setMessageList((prev) => {
+          const findIndex = prev.findIndex((fv) => fv.id === data.id);
+          if (findIndex >= 0) {
+            const updateArr = [...prev];
+            updateArr[findIndex] = data;
+            return updateArr;
+          }
+          return prev;
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   useEffect(() => {
     if (userInfo === null) {
       return;
@@ -178,6 +198,7 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
                   toast({ title: '메시지 등록 실패', position: 'top-right' });
                 }
                 setMessage('');
+                setMessageListFetchTrigger((prev) => !prev);
               }}
             >
               등록
@@ -214,7 +235,7 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
                 photoURL={userInfo.photoURL ?? 'https://bit.ly/broken-link'}
                 isOwner={isOwner}
                 onSendComplete={() => {
-                  setMessageListFetchTrigger((prev) => !prev);
+                  fetchMessageInfo({ uid: userInfo.uid, messageId: messageData.id });
                 }}
               />
             );
