f4539325340c727a20257db1892b4d2aee17d810
spelar <spelar7@gmail.com>
2023-08-09 23:33:21 +0900

    Update auth_user.context.tsx

--- context/auth_user.context.tsx
+++ context/auth_user.context.tsx
@@ -1,9 +1,11 @@
-import useFirebaseAuth from '@/hooks/use_firebase_auth';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React, { createContext, useContext } from 'react';
 import { InAuthUser } from '@/models/in_auth_user';
-import { createContext, useContext } from 'react';
+import useFirebaseAuth from '@/hooks/use_firebase_auth';
 
 interface InAuthUserContext {
   authUser: InAuthUser | null;
+  /** 로그인 여부가 진행중인지 체크 */
   loading: boolean;
   signInWithGoogle: () => void;
   signOut: () => void;
@@ -13,6 +15,7 @@ const AuthUserContext = createContext<InAuthUserContext>({
   authUser: null,
   loading: true,
   signInWithGoogle: async () => ({ user: null, credential: null }),
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   signOut: () => {},
 });
 
